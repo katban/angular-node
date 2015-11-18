2efb0070e41f46e898fce04d314149ea03243b30
katban <grancen@wp.pl>
2015-11-18 10:43:23 +0100

    some refactoring and new feature (delete per api)

--- src/app/app.js
+++ src/app/app.js
@@ -82,8 +82,10 @@
     app.controller('PlacesController', function($scope, $http) {
         $scope.place = [];
 
+        var url = 'http://188.226.184.180:3000/api/';
+
         var getPlaces = function () {
-        $http.get('http://188.226.184.180:3000/api/places')
+        $http.get(url + 'places')
             .then(function (response) {
                 $scope.placesList = response.data;
                     //console.log(response);
@@ -103,13 +105,28 @@
             if (place.name && place.city && place.street && flag == false) {
                 var newPlace = angular.copy(place);
                 //$scope.placesList.push(newPlace);
-                $http.post('http://188.226.184.180:3000/api/places', newPlace)
+                $http.post(url + 'places', newPlace)
                     .then(function (response) {
                         getPlaces();
                     });
             }
-        }
+        };
 
+        $scope.removeItem = function (item) {
+            $http.delete(url + 'place/'+item._id).then(function (response) {
+                getPlaces();
+            });
+        };
+
+        $scope.getPlace = function (id) {
+          $http.get(url + 'place/' + id)
+              .then(function (reponse) {
+                  return response.data;
+              })
+        };
+
+
+        getPlaces();
     });
 
 })();
