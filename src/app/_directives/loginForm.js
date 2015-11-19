20f489c7a139e8226bffeaf8e2bb1b570a6c90c9
katban <grancen@wp.pl>
2015-11-19 11:54:56 +0100

    function to logging

--- src/app/_directives/loginForm.js
+++ src/app/_directives/loginForm.js
@@ -11,8 +11,22 @@ angular.module('Workshop')
             controller: function($scope, $http) {
                 // variables
                 $scope.user = [];
+                var backendUrl = 'http://188.226.184.180:3000/api';
 
+                $scope.loginMessage = '';
                 //methods
+
+                $scope.login = function (user) {
+                    $http.post(backendUrl + '/users', {'username': user.login, 'password': user.password})
+                        .then(function(response) {
+                            console.log(response);
+                            $scope.loginMessage = response.data.message;
+                        }, function errorCallback(error) {
+                            console.log('Błąd: ', error);
+                            $scope.loginMessage = error.data.message;
+                        })
+                }
+
 
             }
         }
