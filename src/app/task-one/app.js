15b8c4a5e715c79762e679538aa94657c3edf205
katban <grancen@wp.pl>
2015-11-22 13:48:32 +0100

    task one: print mock data (bootstrap)

--- src/app/task-one/app.js
+++ src/app/task-one/app.js
@@ -4,7 +4,20 @@
 (function(){
     var app = angular.module('Workshop', []);
 
-    app.controller('BaseController', function () {
+    app.controller('BaseController', function ($scope) {
+        $scope.person = {
+            "fname":"Aely",
+            "lname":"Ruback",
+            "company":"Chaudary Marketing",
+            "phone":"(431)905-3906",
+            "email":"DLinville@elementum.gov",
+            "address":{
+                "streetAddress":"200 Id Ln",
+                "city":"Palm Harbor",
+                "state":"TX",
+                "zip":"99380"
+            }
+        }
 
     });
 })();
