d910e7adc4eb3ca62863e002ec0d097d18e126e8
katban <grancen@wp.pl>
2015-11-22 15:16:10 +0100

    task one: simply print data

--- src/app/task-two/_directives/loginHistory.js
+++ src/app/task-two/_directives/loginHistory.js
@@ -9,6 +9,10 @@ angular.module("Workshop")
             transclude: true,
             scope: {},
             controller: function ($scope, $http) {
+                $http.get('http://www.filltext.com/?rows=50&user={email}&ip={ip}&lastLogin={date|10-10-2004,11-18-2015}')
+                    .then(function (response){
+                        $scope.loginList = response.data;
+                    })
 
             }
         }
